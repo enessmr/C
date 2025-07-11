3899a4543c81ece262213a36095421f50b898f64
enessmr <enessmr22@gmail.com>
2025-07-11 18:18:38 +0300

    starting to mess with myself

--- first_C_program/main.c
+++ first_C_program/main.c
@@ -4,6 +4,11 @@ int main(){
 
     // pov: you really use BullSh1t Code: 💀
 
+    /*
+    * I hate myself BRO
+    * mess with this sh1t lol 
+    */
+
     printf("I like piza!\n");
     printf("Idz Rly Gud!\n");
     return 0;
